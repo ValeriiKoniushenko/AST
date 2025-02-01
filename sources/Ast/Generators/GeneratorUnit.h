b1134e068ee0612e3137091b06090442c687c810
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-02-01 13:46:56 +0200

    Fixed bugs

--- sources/Ast/Generators/GeneratorUnit.h
+++ sources/Ast/Generators/GeneratorUnit.h
@@ -61,20 +61,18 @@ namespace Ast
         {
             return GeneratorUnit(Lexer::typeName);
         }
+        explicit GeneratorUnit(const String& type)
+            : _type{ type } {};
+
         // Don't call this method directly. Override it & implement needed logic
         [[nodiscard]] virtual String OnGenerate(const BaseLexer* lexer) const { return String(); }
 
         [[nodiscard]] virtual String PreGenerate(const BaseLexer* lexer) const { return String(); }
         [[nodiscard]] virtual String PostGenerate(const BaseLexer* lexer) const { return String(); }
-
-    private:
-        explicit GeneratorUnit(const String& type)
-            : _type{ type } {};
     };
 
     template<class T>
-    concept IsGeneratorUnit = std::derived_from<T, GeneratorUnit> && requires(T)
-    {
+    concept IsGeneratorUnit = std::derived_from<T, GeneratorUnit> && requires(T) {
         { T() };
     };
 
