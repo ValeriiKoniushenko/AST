39f01ea4a0db0811d5c2c51ff765ec8e3d6acde0
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-03-05 00:44:04 +0200

    Refactored

--- install.py
+++ install.py
@@ -1,12 +1,16 @@
 from scripts.globals import *
 
-if ProcessGitSafeDir() == False:
-    exit(2)
+def Install():
+    if ProcessGitSafeDir() == False:
+        exit(2)
 
-if ProcessGitSubmodules() == False:
-    exit(3)
+    if ProcessGitSubmodules() == False:
+        exit(3)
 
-if ProcessBoost() == False:
-    exit(4)
+    if ProcessBoost() == False:
+        exit(4)
 
-prints.frameMessage("SUCCESS INSTALL!")
+    prints.frameMessage("SUCCESS INSTALL!")
+
+if __name__ == '__main__':
+    Install()
