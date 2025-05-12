6bee4d76591d59ecb72d8c93e8c76f1bf95a17c5
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-05-12 22:58:41 +0200

    Added new functionality & refactored old code

--- sources/Ast/Utils/Scopes.cpp
+++ sources/Ast/Utils/Scopes.cpp
@@ -1,22 +1,24 @@
-// Copyright (c) 2024 Valerii Koniushenko
+//  MIT License
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+//  Copyright (c) 2019-2025 Valerii Koniushenko
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 #include "Scopes.h"
 
@@ -30,8 +32,15 @@ namespace Ast::Utils
             return nullptr;
         }
 
-        while (*source != 0 && *source != bracket)
+        while (*source != 0)
         {
+            source = TryToSkipAnyQuotes(source);
+
+            if (*source == bracket)
+            {
+                break;
+            }
+
             ++source;
         }
 
@@ -52,8 +61,14 @@ namespace Ast::Utils
         }
 
         std::size_t bracketCounter = 1;
+
         for (; *source != 0 && bracketCounter != 0; ++source)
         {
+            auto* old = source;
+            source = TryToSkipAnyQuotes(source);
+
+            auto diff = old - source;
+
             if (*source == openedBracket)
             {
                 ++bracketCounter;
@@ -64,10 +79,13 @@ namespace Ast::Utils
             }
         }
 
-        return *source != 0 ? --source : nullptr;
+        Assert(bracketCounter == 0);
+
+        return bracketCounter == 0 && *source != 0 ? --source : nullptr;
     }
 
-    const String::CharT* FindClosedBracketR(const String::CharT* source, String::CharT closedBracket, String::CharT openedBracket, const String::CharT* const stopPointBegin)
+    const String::CharT* FindClosedBracketR(const String::CharT* source, String::CharT closedBracket, String::CharT openedBracket,
+                                            const String::CharT* const stopPointBegin)
     {
         if (!Verify(source, "Impossible to find the first bracket because was passed the NULL string"))
         {
@@ -138,6 +156,111 @@ namespace Ast::Utils
         }
 
         return bracketCounter != 0;
+    }
+
+    const String::CharT* TryToSkipAnyQuotes(const String::CharT* src)
+    {
+        if (!src)
+        {
+            return src;
+        }
+
+        if (*src == '\'')
+        {
+            return SkipSingleQuotes(src);
+        }
+        else if (*src == 'R' && src[1] && src[1] == '"' && src[2] && src[2] == '(')
+        {
+            return SkipRawQuotes(src);
+        }
+        else if (*src == '"')
+        {
+            return SkipDoubleQuotes(src);
+        }
+
+        return src;
+    }
+
+    const String::CharT* SkipDoubleQuotes(const String::CharT* src)
+    {
+        if (!src || *src != '"')
+        {
+            return src;
+        }
+
+        ++src;
+
+        String::CharT prev = 0;
+        int counter = 1;
+
+        while (*src && counter != 0)
+        {
+            if (prev != '\\' && *src == '"')
+            {
+                --counter;
+            }
+
+            prev = *src;
+            ++src;
+        }
+
+        Assert(counter == 0);
+
+        return src;
+    }
+
+    const String::CharT* SkipSingleQuotes(const String::CharT* src)
+    {
+        if (!src || *src != '\'')
+        {
+            return src;
+        }
+
+        ++src;
+
+        String::CharT prev = 0;
+        int counter = 1;
+
+        while (*src && counter != 0)
+        {
+            if (prev != '\\' && *src == '\'')
+            {
+                --counter;
+            }
+
+            prev = *src;
+            ++src;
+        }
+
+        Assert(counter == 0);
+
+        return src;
+    }
+
+    const String::CharT* SkipRawQuotes(const String::CharT* src)
+    {
+        if (!src || !(*src == 'R' && src[1] && src[1] == '"' && src[2] && src[2] == '('))
+        {
+            return src;
+        }
+
+        String::CharT prev = 0;
+        int counter = 1;
+
+        while (*src && counter != 0)
+        {
+            if (prev == ')' && *src == '"')
+            {
+                --counter;
+            }
+
+            prev = *src;
+            ++src;
+        }
+
+        Assert(counter == 0);
+
+        return src;
     }
 
 } // namespace Ast::Utils
--- sources/Ast/Utils/Scopes.h
+++ sources/Ast/Utils/Scopes.h
@@ -1,22 +1,24 @@
-// Copyright (c) 2024 Valerii Koniushenko
+//  MIT License
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+//  Copyright (c) 2019-2025 Valerii Koniushenko
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 #pragma once
 
@@ -30,5 +32,10 @@ namespace Ast::Utils
     [[nodiscard]] const String::CharT* FindClosedBracketR(const String::CharT* source, String::CharT closedBracket, String::CharT openedBracket, const String::CharT* const stopPointBegin);
     [[nodiscard]] bool HasUnclosedBracket(const String::CharT* from, const String::CharT* to, String::CharT closedBracket,
                                           String::CharT openedBracket);
+    
+    [[nodiscard]] const String::CharT* TryToSkipAnyQuotes(const String::CharT* src);
+    [[nodiscard]] const String::CharT* SkipDoubleQuotes(const String::CharT* src);
+    [[nodiscard]] const String::CharT* SkipSingleQuotes(const String::CharT* src);
+    [[nodiscard]] const String::CharT* SkipRawQuotes(const String::CharT* src);
 
 } // namespace Ast::Utils
