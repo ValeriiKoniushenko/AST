6ba43b0b05786479f02dd4d8c8aa4303553660e0
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-05-18 23:10:39 +0200

    Added gdb pretty printing

---
+++ gdb-pretties/core_base_string.py
@@ -0,0 +1,21 @@
+class BaseStringPrinter:
+    def __init__(self, val):
+        self.val = val
+
+    def to_string(self):
+        str_ptr = self.val['_string']
+        if str_ptr == 0:
+            return '""'
+        try:
+            str_value = str_ptr.string()
+        except gdb.error:
+            str_value = "<invalid memory>"
+        return f'"{str_value}"'
+
+def PrettyBaseString(val):
+    if str(val.type) == 'Ast::String':
+        return BaseStringPrinter(val)
+    else:
+        return None
+
+gdb.pretty_printers.append(PrettyBaseString)
